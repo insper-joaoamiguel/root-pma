bb71f07ee8c7931a8362a769efeeb41591801c9f
jota <joaopedroamiguel@gmail.com>
2026-05-31 06:30:28 -0300

    gitsubmodules fix

---
+++ init-db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS auth_db    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS account_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS product_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS order_db   CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+GRANT ALL PRIVILEGES ON auth_db.*    TO 'pma'@'%';
+GRANT ALL PRIVILEGES ON account_db.* TO 'pma'@'%';
+GRANT ALL PRIVILEGES ON product_db.* TO 'pma'@'%';
+GRANT ALL PRIVILEGES ON order_db.*   TO 'pma'@'%';
+
+FLUSH PRIVILEGES;
